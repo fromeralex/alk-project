535165f39f98e123306f8d55709fc02db6ddd943
fromeralex <120049734+fromeralex@users.noreply.github.com>
2022-12-07 13:14:21 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-input.onButtonPressed(Button.A, function () {
-    music.playMelody("- - - - - - - - ", 120)
+let strip = neopixel.create(DigitalPin.P1, 10, NeoPixelMode.RGB)
+basic.forever(function () {
+    if (input.acceleration(Dimension.Strength) > 1300) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.pause(500)
+        strip.clear()
+        strip.show()
+    }
 })
+
